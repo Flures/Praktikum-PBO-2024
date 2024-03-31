a75304c9f20cb8c91896d097d91971038a37c2df
Flures <muhammadhamzah764@gmail.com>
2024-03-31 13:16:26 +0800

    posttest 4

---
+++ posttest4/src/main/java/inventory/InventoryManager.java
@@ -0,0 +1,286 @@
+package inventory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class InventoryManager {
+    private static ArrayList<Equipment> equipmentList = new ArrayList<>();
+    private BufferedReader reader;
+
+    public InventoryManager(BufferedReader reader) {
+        this.reader = reader;
+    }
+
+    public void start() throws IOException {
+        int choice;
+        do {
+            displayMenu();
+            System.out.print("Masukkan Pilihan: ");
+            choice = getIntInput();
+            performMenuAction(choice);
+        } while (choice != 0);
+    }
+
+    public void createEquipmentFromInput() {
+        createEquipmentFromInput(null);
+    }
+
+    public void createEquipmentFromInput(String defaultType) {
+        int typeChoice;
+        if(defaultType == null){
+        System.out.println("Pilih Jenis Equipment:");
+        System.out.println("1. Weapon");
+        System.out.println("2. Shield");
+        System.out.println("3. Accessory");
+        System.out.println("Masukkan Pilihan: ");
+        typeChoice = getIntInput();}
+        else{
+            switch(defaultType.toLowerCase()){
+                case "weapon":
+                    typeChoice = 1;
+                    break;
+                case "shield":
+                    typeChoice = 2;
+                    break;
+                case "accessory":
+                    typeChoice = 3;
+                    break;
+                default:
+                    System.out.println("Pilihan Invalid!");
+                    return;
+            }
+        }
+
+        Equipment newEquipment;
+        switch (typeChoice) {
+            case 1:
+                System.out.print("Masukkan Nama Equipment: ");
+                String weaponName = getStringInput();
+                System.out.print("Masukkan Attack Equipment: ");
+                int weaponAttack = getIntInput();
+                System.out.print("Masukkan Defense Equipment: ");
+                int weaponDefense = getIntInput();
+                System.out.print("Masukkan Tipe Weapon: ");
+                String weaponType = getStringInput();
+                System.out.print("Masukkan Weapon Skill: ");
+                String weaponSkill = getStringInput();
+                System.out.print("Masukkan Skill Damage: ");
+                int skillDamage = getIntInput();
+                System.out.print("Masukkan Elemen Weapon: ");
+                String weaponElement = getStringInput();
+                System.out.print("Masukkan Critical Rate: ");
+                int criticalRate = getIntInput();
+                newEquipment = new Weapon(weaponName, weaponAttack, weaponDefense, weaponType, weaponSkill, skillDamage, weaponElement,
+                        criticalRate);
+                break;
+            case 2:
+                System.out.print("Masukkan Nama Equipment: ");
+                String shieldName = getStringInput();
+                System.out.print("Masukkan Attack Equipment: ");
+                int shieldAttack = getIntInput();
+                System.out.print("Masukkan Defense Equipment: ");
+                int shieldDefense = getIntInput();
+                System.out.print("Masukkan Damage Reduction: ");
+                int damageReduction = getIntInput();
+                System.out.print("Masukkan Health Bonus: ");
+                int healthBonus = getIntInput();
+                newEquipment = new Shield(shieldName, shieldAttack, shieldDefense, damageReduction, healthBonus);
+                break;
+            case 3:
+                System.out.print("Masukkan Nama Equipment: ");
+                String accessoryName = getStringInput();
+                System.out.print("Masukkan Attack Equipment: ");
+                int accessoryAttack = getIntInput();
+                System.out.print("Masukkan Defense Equipment: ");
+                int accessoryDefense = getIntInput();
+                System.out.print("Masukkan Tipe Accessory: ");
+                String accessoryType = getStringInput();
+                System.out.print("Masukkan Attack Bonus: ");
+                int attackBonus = getIntInput();
+                System.out.print("Masukkan Healing Bonus: ");
+                int healingBonus = getIntInput();
+                newEquipment = new Accessory(accessoryName, accessoryAttack, accessoryDefense, accessoryType, attackBonus, healingBonus);
+                break;
+            default:
+                System.out.println("Pilihan Invalid!");
+                return;
+        }
+
+        equipmentList.add(newEquipment);
+        System.out.println("Equipment Berhasil Dibuat:");
+    }
+
+    public void updateEquipmentByName() {
+        System.out.print("Masukkan Nama Equipment Yang Ingin Diupdate: ");
+        String name = getStringInput();
+        Equipment foundEquipment = findEquipmentByName(name);
+        if (foundEquipment != null) {
+            System.out.print("Masukkan Nama Baru: ");
+            String newName = getStringInput();
+            System.out.print("Masukkan Attack Baru: ");
+            int newAttack = getIntInput();
+            System.out.print("Masukkan Defense Baru: ");
+            int newDefense = getIntInput();
+            foundEquipment.setName(newName);
+            foundEquipment.setAttack(newAttack);
+            foundEquipment.setDefense(newDefense);
+
+            if (foundEquipment instanceof Weapon) {
+                Weapon weapon = (Weapon) foundEquipment;
+                System.out.print("Masukkan Tipe Weapon Baru: ");
+                String newWeaponType = getStringInput();
+                System.out.print("Masukkan Skill Weapon Baru: ");
+                String newWeaponSkill = getStringInput();
+                System.out.print("Masukkan Skill Damage Baru: ");
+                int newSkillDamage = getIntInput();
+                System.out.print("Masukkan Elemen Weapon Baru: ");
+                String newWeaponElement = getStringInput();
+                System.out.print("Masukkan Critical Rate Baru: ");
+                int newCriticalRate = getIntInput();
+
+                weapon.setWeaponType(newWeaponType);
+                weapon.setWeaponSkill(newWeaponSkill);
+                weapon.setSkillDamage(newSkillDamage);
+                weapon.setWeaponElement(newWeaponElement);
+                weapon.setCriticalRate(newCriticalRate);
+
+            } else if (foundEquipment instanceof Shield) {
+                Shield shield = (Shield) foundEquipment;
+                System.out.print("Masukkan Damage Reduction Baru: ");
+                int newDamageReduction = getIntInput();
+                System.out.print("Masukkan Health Bonus Baru: ");
+                int newHealthBonus = getIntInput();
+                shield.setDamageReduction(newDamageReduction);
+                shield.setHealthBonus(newHealthBonus);
+
+            } else if (foundEquipment instanceof Accessory) {
+                Accessory accessory = (Accessory) foundEquipment;
+                System.out.print("Masukkan Tipe Accessory baru: ");
+                String newAccessoryType = getStringInput();
+                System.out.print("Masukkan Attack Bonus Baru: ");
+                int newAttackBonus = getIntInput();
+                System.out.print("Masukkan Healing Bonus Baru: ");
+                int newHealingBonus = getIntInput();
+                accessory.setAccessoryType(newAccessoryType);
+                accessory.setAttackBonus(newAttackBonus);
+                accessory.setHealingBonus(newHealingBonus);
+            }
+
+            System.out.println("Equipment Berhasil Diupdate!");
+        } else {
+            System.out.println("Equipment tidak ditemukan!");
+        }
+    }
+
+    public void deleteEquipmentByName() {
+        System.out.print("Masukkan Nama Equipment Yang Ingin Dihapus: ");
+        String deleteName = getStringInput();
+        Equipment foundEquipment = findEquipmentByName(deleteName);
+        if (foundEquipment != null) {
+            equipmentList.remove(foundEquipment);
+            System.out.println("Equipment Berhasil Dihapus!");
+        } else {
+            System.out.println("Equipment Tidak Ditemukan!");
+        }
+    }
+
+    public void displayMenu() {
+        System.out.println("Menu:");
+        System.out.println("1. Tambah Equipment");
+        System.out.println("2. Lihat Equipment");
+        System.out.println("3. Update Equipment");
+        System.out.println("4. Hapus Equipment");
+        System.out.println("0. Exit");
+    }
+
+    public void performMenuAction(int choice) {
+        switch (choice) {
+            case 0:
+                System.out.println("Terima Kasih!");
+                System.exit(0);
+                break;
+            case 1:
+                createEquipmentFromInput();
+                break;
+            case 2:
+                if (equipmentList.isEmpty()) {
+                    System.out.println("Tidak Ada Equipment Yang Tersedia Untuk Ditampilkan.");
+                } else {
+                    int counter = 1;
+                    System.out.println("Equipment:");
+                    for (Equipment equipment : equipmentList) {
+                        System.out.println("----------------------------------");
+                        System.out.println("Equipment " + counter++);
+                        if (equipment instanceof Weapon) {
+                            Weapon weapon = (Weapon) equipment;
+                            weapon.displayInfo();
+                        } else if (equipment instanceof Shield) {
+                            Shield shield = (Shield) equipment;
+                            shield.displayInfo();
+                        } else if (equipment instanceof Accessory) {
+                            Accessory accessory = (Accessory) equipment;
+                            accessory.displayInfo();
+                        }
+                        System.out.println("----------------------------------");
+                    }
+                }
+                break;
+            case 3:
+                if (equipmentList.isEmpty()) {
+                    System.out.println("Tidak Ada Equipment Untuk Diupdate.");
+                } else {
+                    updateEquipmentByName();
+                }
+                break;
+            case 4:
+                if (equipmentList.isEmpty()) {
+                    System.out.println("Tidak Ada Equipment Untuk Dihapus.");
+                } else {
+                    deleteEquipmentByName();
+                }
+                break;
+            default:
+                System.out.println("Pilihan Invalid!");
+                break;
+        }
+    }
+
+    private Equipment findEquipmentByName(String name) {
+        for (Equipment equipment : equipmentList) {
+            if (equipment.getName().equals(name)) {
+                return equipment;
+            }
+        }
+        return null;
+    }
+
+    public int getIntInput() {
+        while (true) {
+            try {
+                String input = reader.readLine();
+                return Integer.parseInt(input);
+            } catch (IOException e) {
+                System.out.println("An error occurred while reading input: " + e.getMessage());
+            } catch (NumberFormatException e) {
+                System.out.println("Input harus berupa bilangan bulat.");
+                System.out.print("Coba lagi: ");
+            }
+        }
+    }
+
+    public String getStringInput() {
+        try {
+            String input = reader.readLine();
+            if (input.trim().isEmpty()) {
+                System.out.println("Input tidak boleh kosong.");
+                System.out.print("Coba lagi: ");
+                return getStringInput();
+            }
+            return input;
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading input: " + e.getMessage());
+            return "";
+        }
+    }
+}
